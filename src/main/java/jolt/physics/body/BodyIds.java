d3248b7e4d74f3d063dc9b6e45ee88da50c242fe
aecsocket <aecsocket@tutanota.com>
2023-03-05 21:41:52 +0000

    Add body ID API

--- src/main/java/jolt/physics/body/BodyIds.java
+++ src/main/java/jolt/physics/body/BodyIds.java
@@ -8,8 +8,27 @@ import static jolt.headers.JoltPhysicsC.C_INT;
 public final class BodyIds {
     private BodyIds() {}
 
+    public static final int INVALID_BODY_ID = 0xffffffff;
+    public static final int MAX_BODY_INDEX = 0x7fffff;
+
     public static int at(MemoryAddress address) {
         return address.get(C_INT, 0);
+    }
+
+    public static int index(int id) {
+        return id & MAX_BODY_INDEX;
+    }
+
+    public static byte sequenceNumber(int id) {
+        return (byte) (id >> 24);
+    }
+
+    public static boolean valid(int id) {
+        return id != INVALID_BODY_ID;
+    }
+
+    public static String asString(int id) {
+        return index(id) + "/" + sequenceNumber(id);
     }
 
     public static int[] arrayOf(Collection<? extends Integer> collection) {
