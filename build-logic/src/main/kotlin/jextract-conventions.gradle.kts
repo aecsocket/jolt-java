a221d770e507f20ade70d24f720d0ccd1a5fce29
aecsocket <aecsocket@tutanota.com>
2023-03-05 13:10:33 +0000

    Remove Kotlin module, gradle-jextract 0.3.2

--- build-logic/src/main/kotlin/jextract-conventions.gradle.kts
+++ build-logic/src/main/kotlin/jextract-conventions.gradle.kts
@@ -1,9 +1,4 @@
-import io.github.krakowski.jextract.JextractPlugin
-
 plugins {
     id("java-conventions")
-    //id("io.github.krakowski.jextract")
+    id("io.github.krakowski.jextract")
 }
-
-// TODO: https://github.com/krakowski/gradle-jextract/pull/14
-apply<JextractPlugin>()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,6 @@ plugins {
 rootProject.name = "jolt-java"
 
 include("jolt-java-headers")
-include("jolt-java-kotlin")
 include("jolt-java-natives-linux-x86")
 include("jolt-java-natives-windows-x86")
 include("jolt-java-natives-macos-x86")
